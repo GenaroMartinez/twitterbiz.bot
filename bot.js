c0b33b61b7c535e9199a6d36cdb8ea9ed4c471da
Naro <36895317+GenaroMartinez@users.noreply.github.com>
2020-06-28 07:09:51 -0700

    changed the dotenv removed I put all hidden keys in gitignore

--- bot.js
+++ bot.js
@@ -1,18 +1,12 @@
 
 const Twit = require('twit');
-
-require('dotenv').config()
-
+const config = require ('./config.js');
 
 
-const T = new Twit({
-    consumer_key: process.env.BOT_CONSUMER,
-    consumer_secret: process.env.BOT_CONSUMERSECRET,
-    access_token: process.env.BOT_ACCESS,
-    access_token_secret: process.env.BOT_ACCESS_SECRET,
-    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL: true,     // optional - requires SSL certificates to be valid.
-});
+
+
+
+const T = new Twit(config);
 
 T.post('statuses/update', { status: 'hello world!' }, function (
     err,
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const tokens = ({
+module.exports = {
     consumer_key:'3ZdtoSCUtJdkZxMJUsTx4JlXQ',
     consumer_secret:      'SGXRns0Fqg7WEHZvQ3PCqP0cnjkFgMzddSqD8HdrAq4cFOGebj',
     access_token:         '1275263857980616705-tAJbyWbYelltFbNHCDbg5p4zObXoqJ',
     access_token_secret:  'LLkKmfstUgX6DIrwYJ2C0GyTKkom8qQBE1reOsm5f0GwL',
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL:            true,     // optional - requires SSL certificates to be valid.
-  });
+  };
